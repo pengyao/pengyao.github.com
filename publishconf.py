ec9d3282445be1b86ae3d73f12c2fb2cba0eacc3
pengyao <pengyao@pengyao.org>
2013-04-10 14:24:53 +0800

    初始化pelican

---
+++ publishconf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import sys
+sys.path.append('.')
+from pelicanconf import *
+
+SITEURL = 'pengyao.org[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[Chttp://[C[C[Cpengyao.org'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+# Uncomment following line for absolute URLs in production:
+#RELATIVE_URLS = False
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
