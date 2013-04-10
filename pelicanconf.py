37f81d98fc0ffd789926cf601c48aaf49cd893b4
pengyao <pengyao@pengyao.org>
2013-04-10 18:09:31 +0800

    调整配置

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ DEFAULT_DATE = (2013,01,01,01,01,01)
 
 FILES_TO_COPY = (('extra/favicon.ico', 'favicon.ico'),
                  ('extra/CNAME', 'CNAME'),
-                 ('extra/README.md', 'README.md'),)
+                 ('extra/README', 'README'),)
 
 MD_EXTENSIONS = ['codehilite', 'extra']
 OUTPUT_PATH = 'output/'
